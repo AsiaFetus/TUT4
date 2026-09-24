05563bd299e1ce4ef482ee5f3be40cf2fc68b99d
Georges Nguyen <georges06nguyen@gmail.com>
2026-09-24 15:23:12 +0200

    fixed parentheses error

--- compiles_with_warning.cxx
+++ compiles_with_warning.cxx
@@ -4,7 +4,7 @@ int main() {
     int i = 3;
     std::cout << "The value of the variable i is " << i << '\n';
 
-    if (i = 4) {
+    if ((i = 4)) {
         std::cout << "I have entered the code block so i must be equal to 4\n";
         std::cout << "This shouldn't happen \n";
     }
